dc2a51463c998913963b6372e2b93fa028a82128
İbrahim Ediz <Kurs>
2019-11-30 15:20:57 +0200

    30112019_3

--- Opencv.py
+++ Opencv.py
@@ -3,17 +3,14 @@ import numpy as np
 
 
 
-img = cv2.imread('indir.jpg',cv2.IMREAD_GRAYSCALE)
+img = cv2.imread('bookpage.jpg')
 
-px = img[50:80,30:55]
-print(px)
+# px = img[50:80,30:55]
+# print(px)
 # print(img.size)
 # print(img.shape)
 # print(img.dtype)
-
-img[0:30,0:25] = px
-
-img[50:80,30:55] = [1]
+ 
 
 # cv2.line(img,(0,0),(150,150),(59,116,249),5)
 
@@ -24,7 +21,13 @@ img[50:80,30:55] = [1]
 # font = cv2.FONT_HERSHEY_SIMPLEX
 # cv2.putText(img,"Vektorel",(0,60),font,2,(237,136,249),3)
 
+grayscaled = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+px = grayscaled[0:50,0:50]
+print(px)
+th = cv2.adaptiveThreshold(grayscaled,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,115,1)
+# retval ,treshold = cv2.threshold(grayscaled,20,255,cv2.THRESH_BINARY)
 
 cv2.imshow('resim',img)
+cv2.imshow('eşikleme',th)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
