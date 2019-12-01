5c47605175025917d73e35578e70e7f6055c29a3
İbrahim Ediz <Kurs>
2019-12-01 14:14:33 +0200

    01/12/2019

--- Opencv2.py
+++ Opencv2.py
@@ -1,12 +1,18 @@
 import cv2
 import numpy as np
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("videooo.mp4")
+
+fgbg = cv2.createBackgroundSubtractorMOG2()
 
 while(1):
     try:
         _, frame = cap.read()
 #####################################
-        
+        cv2.imshow('den',frame)
+        fgmask = fgbg.apply(frame)
+
+        cv2.imshow('REs',frame)
+        cv2.imshow('Maske',fgmask)
 
 ####################################
         k = cv2.waitKey(1) & 0xFF
